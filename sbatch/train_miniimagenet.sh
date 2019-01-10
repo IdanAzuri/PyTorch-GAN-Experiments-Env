2eeb3b38642c296505417cfa5cab78683b28392d
idanazuri <idan.azuri@github.com>
2019-01-10 19:22:55 +0200

    minor

--- sbatch/train_miniimagenet.sh
+++ sbatch/train_miniimagenet.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-#SBATCH --mem=30g
-#SBATCH -c 15
-#SBATCH --gres=gpu:4
-#SBATCH --time=5-00
+#SBATCH --mem=20g
+#SBATCH -c 10
+#SBATCH --gres=gpu:2
+#SBATCH --time=2-00
 #SBATCH --mail-user=idan.azuri@mail.huji.ac.il
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
+#SBATCH --array=1-4%4
+
 
 module load torch
 module load tensorflow
 module load nvidia
 
-dir=/cs/labs/daphna/idan.azuri/PyTorch-GAN-Experiments-Env
+dir=/cs/snapless/daphna/idan.azuri/PyTorch-GAN-Experiments-Env
 
 cd $dir
+
 source /cs/labs/daphna/idan.azuri/venv3.6/bin/activate
 
-
-python3 main.py --config miniimagenet_config
+#python3 main.py --config pretrained_vgg19_bn_${SLURM_ARRAY_TASK_ID}
+python3 main.py --config pretrained_resnet50_${SLURM_ARRAY_TASK_ID}
