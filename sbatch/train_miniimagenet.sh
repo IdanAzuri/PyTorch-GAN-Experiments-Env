b51b7ac70e5bf27a38bb57c0e2da816939f7cd17
idanazuri <idan.azuri@gmail.com>
2019-04-12 15:21:43 +0300

    sbatch

--- sbatch/train_miniimagenet.sh
+++ sbatch/train_miniimagenet.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --mem=4g
+#SBATCH --mem=6g
 #SBATCH -c 8
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:m60:1
 #SBATCH --time=1-20
 #SBATCH --mail-user=idan.azuri@mail.huji.ac.il
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
-#SBATCH --array=0-6%7
+#SBATCH --array=0-4%5
 
 
 module load torch
@@ -19,32 +19,31 @@ source /cs/labs/daphna/idan.azuri/venv3.6/bin/activate
 
 
 if [ $SLURM_ARRAY_TASK_ID -eq 0 ]; then
-python3 main.py --config 4conv
+python3 main.py --config 4conv --mode predict
 fi
 
 if [ $SLURM_ARRAY_TASK_ID -eq 1 ]; then
-python3 main.py --config 4conv_transductive
+echo $SLURM_ARRAY_TASK_ID
+    python3 main.py --config resnet50 --mode predict
 fi
 
 if [ $SLURM_ARRAY_TASK_ID -eq 2 ]; then
-python3 main.py --config resnet50
+python3 main.py --config pretrained_resnet50_aug --mode predict
 fi
 
 if [ $SLURM_ARRAY_TASK_ID -eq 3 ]; then
-python3 main.py --config pretrained_resnet50
+python3 main.py --config pretrained_vgg19_bn_2 --mode predict
 fi
 
 if [ $SLURM_ARRAY_TASK_ID -eq 4 ]; then
-python3 main.py --config resnet50_aug
+python3 main.py --config pretrained_vgg19_bn_2_aug --mode predict
 fi
 
-if [ $SLURM_ARRAY_TASK_ID -eq 5 ]; then
-python3 main.py --config pretrained_resnet50_aug
-fi
+# if [ $SLURM_ARRAY_TASK_ID -eq 5 ]; then
+# python3 main.py --config pretrained_resnet50 --mode predict
+# fi
 
-#if [ $SLURM_ARRAY_TASK_ID -eq 6 ]; then
-#python3 main.py --config 4conv_aug
-#fi
+
 
 #python3 main.py --config pretrained_vgg19_bn_${SLURM_ARRAY_TASK_ID}
 #python3 main.py --config pretrained_resnet50_${SLURM_ARRAY_TASK_ID}
