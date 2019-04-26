fa523ecff887a27734a79fa8220b32734a28d471
idanazuri <idan.azuri@gmail.com>
2019-04-26 12:14:30 +0300

    debugging

--- one_shot_aug/__init__.py
+++ one_shot_aug/__init__.py
@@ -79,6 +79,13 @@ class OneShotAug():
 			self.meta_net_base = self.meta_net
 			self.title = self.meta_net.title
 			self.meta_net = self.meta_net.model
+			# Find total parameters and trainable parameters
+			total_params = sum(p.numel() for p in self.meta_net.parameters())
+			print(f'{total_params:,} total parameters.')
+			total_trainable_params = sum(
+				p.numel() for p in model.parameters() if p.requires_grad)
+			print(f'{total_trainable_params:,} training parameters.')
+
 		else:
 			self.meta_net = MiniImageNetModel()
 			self.title = self.meta_net.title
